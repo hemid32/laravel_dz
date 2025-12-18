4d953616df82ae2df0acc889a2b3776abd34c7f3
hemid32 <benameurhemidi@gmail.com>
2025-12-18 23:31:44 +0100

    add api key

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ApiKeyMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+             $middleware->append(ApiKeyMiddleware::class);
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::group([
 
     Route::post('login',[AuthController::class , 'login']);
     Route::post('logout', 'AuthController@logout');
-    Route::post('register', [AuthController::class , 'register']);
+    Route::post('register', [AuthController::class , '']);
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
 
